6a511f1d2676de6dc0e92f88b2952abc3d27582a
bps10 <brian.patrick.schmidt@gmail.com>
2013-02-26 18:59:31 -0800

    colorModel update

---
+++ presentations/static/js/slide.js
@@ -0,0 +1,23 @@
+slide = function() {
+	var slide = {version: "0.0.1"}, slide_document = document, slide_window = window;
+	formatImages = function (percentage) {
+		for( i=0; i < document.images.length; i++)
+		{ 
+			width = document.images[i].width * percentage / 100.0;
+			height = document.images[i].height  * percentage / 100.0;
+			
+			window.document.images[i].setAttribute("width",width);
+			window.document.images[i].setAttribute("height",height);
+
+		}
+	};
+
+	formatImage = function (percentage) {
+		img = window.document.getElementById(imgId);
+		width = img.width * percentage / 100.0;
+		height = img.height  * percentage / 100.0;
+		
+		img.setAttribute("width",width);
+		img.setAttribute("height",height);
+	};
+}();
