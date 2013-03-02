c79b95a986236b1ef5df8668aa00db4f2c71325e
bps10 <brian.patrick.schmidt@gmail.com>
2013-03-02 15:27:37 -0800

    added analytics

--- static/js/repo.js
+++ static/js/repo.js
@@ -1,10 +1,9 @@
-<div class="container-fluid">
-<div class="row-fluid">
-<div class="span2">
-<!--Sidebar content-->
-</div>
-<div class="span10">
-<!--Body content-->
-</div>
-</div>
-</div>
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-38977206-1']);
+_gaq.push(['_trackPageview']);
+
+(function() {
+ var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+ ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+ var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+ })();
